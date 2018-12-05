3868a7eb3f494c4137a44a747e63c53d382e4070
Jens-Ole Skydt Andersen <josa@campusvejle.dk>
2018-12-05 16:01:46 +0100

    possibly solved part two?, takes a while to calculate this...

--- Solutions/Day5.cs
+++ Solutions/Day5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Utils;
 
@@ -13,8 +14,13 @@ namespace Solutions
             input = utils.ReadFileAsStringArray()[0];
         }
 
-        public override string PartOne()
-        {
+        //public override string PartOne()
+        //{
+        //    string input = this.input;
+        //    return RemovePairs(input).Length.ToString();
+        //}
+
+        private string RemovePairs(string input){
             bool noMatches = true;
             while (noMatches)
             {
@@ -34,14 +40,43 @@ namespace Solutions
                         input = input.Remove(index, 2);
                         break;
                     }
-                    
+
                     index++;
                 }
                 if (index + 1 == input.Length) noMatches = false;
             }
 
 
-            return input.Length.ToString();
+            return input;
+        }
+
+        public override string PartTwo()
+        {
+            string input = this.input;
+
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            Dictionary<char, int> withExceptCharC = new Dictionary<char, int>();
+
+            foreach (char c in alphabet)
+            {
+                var tempInput = input.Replace(c.ToString(), string.Empty);
+                tempInput = tempInput.Replace(char.ToLower(c).ToString(), string.Empty);
+                withExceptCharC.Add(c, RemovePairs(tempInput).Count());
+            }
+
+            KeyValuePair<char, int> lowestPair = withExceptCharC.First();
+
+            foreach (var item in withExceptCharC)
+            {
+                if (item.Value < lowestPair.Value)
+                {
+                    lowestPair = item;
+                }
+            }
+
+
+            return lowestPair.Value.ToString();
         }
     }
 }
